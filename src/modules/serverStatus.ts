82260203ee76a7e5db825e98506037ff5477f162
Klaus Dieter Kupper <klausdk1999@gmail.com>
2023-06-04 18:14:35 -0300

    remove unused modules

--- src/modules/serverStatus.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyRequest , FastifyReply } from "fastify";
-
-export async function serverStatus(
-  _: FastifyRequest,
-  reply: FastifyReply
-) {
-  return reply.status(200).send({ message: `Servidor online.` });
-}
